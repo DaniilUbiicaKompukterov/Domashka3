cf3cbf6ebbee08c408f924b3674693f4ac00c77d
Daniil Tikhonov <74093201+DaniilUbiicaKompukterov@users.noreply.github.com>
2020-11-28 10:02:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 1-е задание
-myList = [1, 2, 3]                  # числа
-myNewList = [i * 2 for i in myList] # умножение чисел на 2
-print(myNewList)                    # вывод новых чисел
+lst = [1, 2, 3]                  # числа
+newLst = [i * 2 for i in lst] # умножение чисел на 2
+print(newLst)                    # вывод новых чисел
 
 
 # 2-е задание
