2e420ade1b822b3e03c2f1f36e33c582893ae083
Daniil Tikhonov <74093201+DaniilUbiicaKompukterov@users.noreply.github.com>
2020-11-28 08:40:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-'''
-my_list = [1, 2, 3]
-my_new_list = [i * 2 for i in my_list]
-print(my_new_list)
-'''
+# 1-е задание
+myList = [1, 2, 3]
+myNewList = [i * 2 for i in myList]
+print(myNewList)
 
-'''
-my_list = [1, 2, 3]
-my_new_list = [i ** 2 for i in my_list]
-print(my_new_list)
-'''
 
-'''
-list = 'Hello world'
-for i in range(len(list)):
-    if list[i] == " ":
-        list = list.replace(list[i + 1], list[i + 1].upper(), 1)
-print(list)
-'''
+# 2-е задание
+myList = [1, 2, 3]
+myNewList = [i ** 2 for i in myList]
+print(myNewList)
 
-'''
+
+
+
+# 3-е задание
 list = 'Hello world'
 for i in range(len(list)):
     if list[i] == " ":
         list = list.replace("w", "W")
 print(list)
-'''
 
-'''
+
+# 4-е задание
 from datetime import datetime
 
 year1 = datetime.now().year
 year2 = 1900
-rad = year1 - year2
-for i in range(rad+1):
+ren = year1 - year2
+for i in range(ren+1):
     print(year2 + i)
-'''
+
 
