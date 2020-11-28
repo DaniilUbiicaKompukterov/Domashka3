b43f6ef7d7cc6eece9e6f2f85479863f0cd5e446
Daniil Tikhonov <74093201+DaniilUbiicaKompukterov@users.noreply.github.com>
2020-11-28 09:37:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ print(myNewList)                    # вывод новых чисел
 
 
 # 2-е задание
-myList = [1, 2, 3]                   # числа
-myNewList = [i ** 2 for i in myList] # возведение чисел во 2-ю степень
-print(myNewList)                     # вывод новых чисел
+mylist = [1,2,3]  # числа
+counter = 0       # счетчик   
+for i in mylist:  # цикл 
+    counter+=i*i  # числа записываются в счётчик в квадрате и складываются
+print(counter)    # вывод нового числа
 
 
 
 
 # 3-е задание
-list = 'Hello world'                  # текст
-for i in range(len(list)):            # цикл по индексам
-    if list[i] == " ":                # поиск пробела 
+list = 'Hello world'                                      # текст
+for i in range(len(list)):                                # цикл по индексам
+    if list[i] == " ":                                    # поиск пробела 
         list = list.replace("Hello world", "HELLO WORLD") # если пробел есть , то всё преходит в верхний регистр
-print(list)                           # вывод результата
+print(list)                                               # вывод результата
 
 
 # 4-е задание
