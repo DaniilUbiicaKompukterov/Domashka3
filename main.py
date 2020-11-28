1e491dc4f595f09da0483034b7a043b0997dbd09
Daniil Tikhonov <74093201+DaniilUbiicaKompukterov@users.noreply.github.com>
2020-11-28 08:58:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 # 1-е задание
-myList = [1, 2, 3]
-myNewList = [i * 2 for i in myList]
-print(myNewList)
+myList = [1, 2, 3]                  # числа
+myNewList = [i * 2 for i in myList] # умножение чисел на 2
+print(myNewList)                    # вывод новых чисел
 
 
 # 2-е задание
-myList = [1, 2, 3]
-myNewList = [i ** 2 for i in myList]
-print(myNewList)
+myList = [1, 2, 3]                   # числа
+myNewList = [i ** 2 for i in myList] # возведение чисел во 2-ю степень
+print(myNewList)                     # вывод новых чисел
 
 
 
 
 # 3-е задание
-list = 'Hello world'
-for i in range(len(list)):
-    if list[i] == " ":
-        list = list.replace("w", "W")
-print(list)
+list = 'Hello world'                  # текст
+for i in range(len(list)):            # цикл по индексам
+    if list[i] == " ":                # поиск пробела 
+        list = list.replace("w", "W") # если пробел есть , то w меняется на W
+print(list)                           # вывод результата
 
 
 # 4-е задание
-from datetime import datetime
 
-year1 = datetime.now().year
-year2 = 1900
-ren = year1 - year2
-for i in range(ren+1):
-    print(year2 + i)
+for i in range(1900, 2021):           # диапозон годов
+    print(i)                          # вывод
 
 
